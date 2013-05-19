f904ea990b7c58554603579f3ba1ab20fd1ab6ff
Alistair McKinlay <ali@Zeus.(none)>
2013-05-19 21:54:53 +0100

    Re-added the config file with non-specific password ;-)

---
+++ helpers/config.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+     * This is a page that includes a few config constants used 
+     * throughout the app. Currently, just mainly database stuff.
+     */
+
+    $db_host = 'localhost';
+    $db_user = 'username';
+    $db_pass = 'password';
+    $db_name = 'schedulesnap';
+?>
+
